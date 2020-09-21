7f6a399b82fd21d04a188660722596c24fa06b7b
Adam Bresler <bresler.adam@gmail.com>
2020-09-21 17:24:15 +0200

    Update Preditive_creation.R

--- Preditive_creation.R
+++ Preditive_creation.R
@@ -11,7 +11,7 @@ data <- read.csv("rolling_average_all.csv")
 data <- data[,-1]
 
 #Eventually, add this into loop
-#colnames(data)[c(10,11)] <- c("average_serve_rating", "average_return_rating")
+colnames(data)[c(57:98)] <- paste("rolling_average", colnames(data[15:56]), sep="_")
 
 
 predictive <- data %>%
@@ -23,15 +23,17 @@ predictive2 <- data %>%
 
 predictive3 <- predictive[seq(1,52522,2),-c(57:98)]
 
+# Trying to merge them -------------------------------------------------------
 
-first_player<-predictive2[seq(1,52522,2),-1]
-second_player<-predictive2[seq(2,52522,2),-1]
+first_player<-predictive[seq(1,52522,2),]
+second_player<-predictive[seq(2,52522,2),]
 
-difference <- first_player-second_player
+colnames(first_player)[c(5,15:98)] <- paste("Player_A", colnames(first_player[c(5,15:98)]), sep="_")
+colnames(second_player)[c(5,15:98)] <- paste("Player_B", colnames(second_player[c(5,15:98)]), sep="_")
 
-predictive_dataset <- cbind(predictive3,difference)
+second_player <- second_player[, -c(1:4, 6:14)]
 
-rm(predictive, predictive2, predictive3, second_player, first_player, difference)
+predictive_dataset <- cbind(first_player, second_player)
 
 colnames(predictive_dataset)[c(2,3)] <- c("Player_A", "Player_B")
 
@@ -39,25 +41,43 @@ predictive_dataset$wl <- ifelse(predictive_dataset$wl == 'winner', "Player A", "
 
 predictive_dataset$wl <- as.factor(predictive_dataset$wl)
 
-# Adding the seeding ---------------------------------------------------------
+predictive_dataset <- predictive_dataset[, c(1:5, 99, 6:98, 100:183)] #Watch out for this! Change as data changes
 
-predictive <- data %>%
-  group_by(Match_ID)%>%arrange(.by_group = TRUE)
-
-first_player<-predictive[seq(1,52522,2),-1]
-player_1_seed <-first_player$seed
-
-second_player<-predictive[seq(2,52522,2),-1]
-player_2_seed <-second_player$seed
-
-rm(predictive)
-
-predictive_dataset$player_A_seed <- player_1_seed
-predictive_dataset$player_B_seed <- player_2_seed
-
-predictive_dataset <- predictive_dataset[,c(1:4,99:100,6:98)]
-
-rm(first_player,second_player)
+# ----------------------------------------------------------------------------
+# first_player<-predictive2[seq(1,52522,2),-1]
+# second_player<-predictive2[seq(2,52522,2),-1]
+# 
+# difference <- first_player-second_player
+# 
+# predictive_dataset <- cbind(predictive3,difference)
+# 
+# rm(predictive, predictive2, predictive3, second_player, first_player, difference)
+# 
+# colnames(predictive_dataset)[c(2,3)] <- c("Player_A", "Player_B")
+# 
+# predictive_dataset$wl <- ifelse(predictive_dataset$wl == 'winner', "Player A", "Player B")
+# 
+# predictive_dataset$wl <- as.factor(predictive_dataset$wl)
+# 
+# # Adding the seeding ---------------------------------------------------------
+# 
+# predictive <- data %>%
+#   group_by(Match_ID)%>%arrange(.by_group = TRUE)
+# 
+# first_player<-predictive[seq(1,52522,2),-1]
+# player_1_seed <-first_player$seed
+# 
+# second_player<-predictive[seq(2,52522,2),-1]
+# player_2_seed <-second_player$seed
+# 
+# rm(predictive)
+# 
+# predictive_dataset$player_A_seed <- player_1_seed
+# predictive_dataset$player_B_seed <- player_2_seed
+# 
+# predictive_dataset <- predictive_dataset[,c(1:4,99:100,6:98)]
+# 
+# rm(first_player,second_player)
 
 #write.csv(predictive_dataset, file = "C:/Users/Adam Bresler/Documents/GitHub/Statistics-Honours-Project/Data/all_differences_no_custom_features.csv")
 
