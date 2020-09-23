6d4f823bb8d67de7723c08b70e459b09bcc09ceb
LukaE98 <68850238+LukaE98@users.noreply.github.com>
2020-09-23 16:39:42 +0200

    Update RA_surface and H2H.R

--- RA_surface and H2H.R
+++ RA_surface and H2H.R
@@ -161,6 +161,185 @@ indices2 <- which(H_2_H$name == "David Ferrer" & H_2_H$Opponent  == "Andy Murray
 H_2_H[indices2,c(2,3,4,141)]
 
 
+indices_overall <- which(H_2_H$name == "Rafael Nadal" & H_2_H$Opponent  == "Novak Djokovic" )
+
+H_2_H[indices_overall,c(2,3,4,141)]
+
 length(which(is.na(H_2_H$head_to_head_record)))
 
+# head to head per court surface ---------------------------------------------
 
+
+clay <- H_2_H[which(H_2_H$tournament_surface=="Clay"),]
+
+grass <-H_2_H[which(H_2_H$tournament_surface=="Grass"),]
+
+hard_court <-H_2_H[which(H_2_H$tournament_surface=="Hard"),]
+
+
+H_2_H$head_to_head_record_court_surface <- 0
+clay$head_to_head_record_court_surface <- 0
+grass$head_to_head_record_court_surface <- 0
+hard_court$head_to_head_record_court_surface <- 0
+
+
+# clay
+
+head_to_head_clay <- function(player1, player2){
+  ind <- which(clay$name == player1 & clay$Opponent  == player2 )
+  
+  if(is.empty(ind)|length(ind)==1){
+    return(list("Players have never met before.",ind))
+  }
+  else{
+    
+    return(ind)
+  }
+  
+}
+
+for (i in unique(clay$name)){
+  ind_clay <- which(clay$name == i)
+  opponents <- unique(clay$Opponent[ind_clay])
+  
+  for (j in 1:length(opponents)){
+    matches <- head_to_head_clay(i,opponents[j])
+    if (matches[1]=="Players have never met before."){
+      clay$head_to_head_record_court_surface[matches[[2]]] <- NA
+    }
+    else{
+      wins=0
+      total=0
+      clay$head_to_head_record_court_surface[matches[1]]<-NA
+      for (k in 2:length(matches)){
+        total=total+1
+        if(clay$wl[matches[k-1]]=="winner"){
+          wins=wins+1 
+        }
+        h2h_percent=100*(wins/total) 
+        clay$head_to_head_record_court_surface[matches[k]]<-h2h_percent
+      }
+    }
+  }
+}
+
+
+indices_clay <- which(clay$name == "Rafael Nadal" & clay$Opponent  == "Novak Djokovic" )
+
+clay[indices_clay,c(2,3,4,142)]
+
+# grass
+
+head_to_head_grass <- function(player1, player2){
+  ind <- which(grass$name == player1 & grass$Opponent  == player2 )
+  
+  if(is.empty(ind)|length(ind)==1){
+    return(list("Players have never met before.",ind))
+  }
+  else{
+    
+    return(ind)
+  }
+  
+}
+
+for (i in unique(grass$name)){
+  ind_grass <- which(grass$name == i)
+  opponents <- unique(grass$Opponent[ind_grass])
+  
+  for (j in 1:length(opponents)){
+    matches <- head_to_head_grass(i,opponents[j])
+    if (matches[1]=="Players have never met before."){
+      grass$head_to_head_record_court_surface[matches[[2]]] <- NA
+    }
+    else{
+      wins=0
+      total=0
+      grass$head_to_head_record_court_surface[matches[1]]<-NA
+      for (k in 2:length(matches)){
+        total=total+1
+        if(grass$wl[matches[k-1]]=="winner"){
+          wins=wins+1 
+        }
+        h2h_percent=100*(wins/total) 
+        grass$head_to_head_record_court_surface[matches[k]]<-h2h_percent
+      }
+    }
+  }
+}
+
+
+indices_grass <- which(grass$name == "Rafael Nadal" & grass$Opponent  == "Novak Djokovic" )
+
+grass[indices_grass,c(2,3,4,142)]
+
+# hard court
+
+head_to_head_hard_court <- function(player1, player2){
+  ind <- which(hard_court$name == player1 & hard_court$Opponent  == player2 )
+  
+  if(is.empty(ind)|length(ind)==1){
+    return(list("Players have never met before.",ind))
+  }
+  else{
+    
+    return(ind)
+  }
+  
+}
+
+for (i in unique(hard_court$name)){
+  ind_hard_court <- which(hard_court$name == i)
+  opponents <- unique(hard_court$Opponent[ind_hard_court])
+  
+  for (j in 1:length(opponents)){
+    matches <- head_to_head_hard_court(i,opponents[j])
+    if (matches[1]=="Players have never met before."){
+      hard_court$head_to_head_record_court_surface[matches[[2]]] <- NA
+    }
+    else{
+      wins=0
+      total=0
+      hard_court$head_to_head_record_court_surface[matches[1]]<-NA
+      for (k in 2:length(matches)){
+        total=total+1
+        if(hard_court$wl[matches[k-1]]=="winner"){
+          wins=wins+1 
+        }
+        h2h_percent=100*(wins/total) 
+        hard_court$head_to_head_record_court_surface[matches[k]]<-h2h_percent
+      }
+    }
+  }
+}
+
+
+indices_hard_court <- which(hard_court$name == "Rafael Nadal" & hard_court$Opponent  == "Novak Djokovic" )
+
+# combining and adding to overall data set 
+
+
+grass_H2H <- grass[,c(1,2,3,4,6,7,9,12,142)]
+clay_H2H <- clay[,c(1,2,3,4,6,7,9,12,142)]
+hard_court_H2H <- hard_court[,c(1,2,3,4,6,7,9,12,142)]
+
+court_H2H <- rbind(grass_H2H,clay_H2H,hard_court_H2H)
+
+
+court_H2H <-court_H2H %>%
+  group_by(tournament_date)%>%arrange(.by_group = TRUE)
+
+for (i in unique(court_H2H$tourney_id)){
+  indices <- which(court_H2H$tourney_id==i)
+  court_H2H[indices,] <- court_H2H[indices,]%>%arrange(desc(Match_order))
+}
+
+H_2_H$head_to_head_record_court_surface <- court_H2H$head_to_head_record_court_surface
+
+
+indices <- which(H_2_H$name == "Rafael Nadal" & H_2_H$Opponent  == "Novak Djokovic" )
+
+H_2_H[indices,c(2,4,12,141,142)]
+
+str(H_2_H$head_to_head_record)
+str(H_2_H$head_to_head_record_court_surface)
