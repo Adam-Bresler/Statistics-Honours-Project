11452de2223fea3ff1a6c741efda094c55745c40
Adam Bresler <bresler70@gmail.com>
2020-11-25 10:45:19 +0200

    Update Random_forest_final.R

--- Random_forest_final.R
+++ Random_forest_final.R
@@ -4,8 +4,16 @@ library(lubridate)
 library(magrittr)
 library(caret)
 library(stats)
+library(randomForest)
 
-# Using the raw data --------------------------------------------------------
+# Defining ranger grid -------------------------------------------------------
+
+ctrl <- trainControl(method = 'cv', number = 5, verboseIter = T)
+rf_grid <- expand.grid(mtry = c(2:7),
+                       splitrule = 'gini',
+                       min.node.size = c(1:5))
+
+# Using the raw data ---------------------------------------------------------
 # raw rolled -----------------------------------
 data <- read.csv("raw_rolled.csv")
 data <- data[,-1]
@@ -20,8 +28,6 @@ ind <- 1:23656
 
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
-
-library(randomForest)
 
 set.seed(2020)
 rf_tennis_raw_rolled <- randomForest(as.formula(paste(colnames(data)[4], "~",
@@ -41,6 +47,24 @@ table(rf_pred_raw_rolled, test_data$wl)
 
 varImpPlot(rf_tennis_raw_rolled, type = 2)
 
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_raw_rolled <- train(as.formula(paste(colnames(data)[4], "~",
+                                    paste(colnames(data)[c(5:6, 16:75)], collapse = "+"),
+                                    sep = "")), data = train_data,
+                                    method = 'ranger',
+                                    num.trees = 350,
+                                    trControl = ctrl,
+                                    verbose = F,
+                                    tuneGrid = rf_grid)
+
+rf_caret_pred_raw_rolled <- predict(rf_caret_tennis_raw_rolled, test_data)
+rf_caref_cf_raw_rolled <- confusionMatrix(rf_caret_pred_raw_rolled, test_data$wl)
+sum(diag(rf_caref_cf_raw_rolled$table))/sum(rf_caref_cf_raw_rolled$table)
+
+
 # Raw Rolled BC --------------------------------
 data <- read.csv("raw_rolled_bc.csv")
 data <- data[,-1]
@@ -55,8 +79,6 @@ ind <- 1:23656
 
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
-
-library(randomForest)
 
 set.seed(2020)
 rf_tennis_raw_rolled_bc <- randomForest(as.formula(paste(colnames(data)[4], "~",
@@ -76,6 +98,24 @@ table(rf_pred_raw_rolled_bc, test_data$wl)
 
 varImpPlot(rf_tennis_raw_rolled_bc, type = 2)
 
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_raw_rolled_bc <- train(as.formula(paste(colnames(data)[4], "~",
+                                                     paste(colnames(data)[c(5:6, 16:75)], collapse = "+"),
+                                                     sep = "")), data = train_data,
+                                    method = 'ranger',
+                                    num.trees = 350,
+                                    trControl = ctrl,
+                                    verbose = F,
+                                    tuneGrid = rf_grid)
+
+rf_caret_pred_raw_rolled_bc <- predict(rf_caret_tennis_raw_rolled_bc, test_data)
+rf_caref_cf_raw_rolled_bc <- confusionMatrix(rf_caret_pred_raw_rolled_bc, test_data$wl)
+sum(diag(rf_caref_cf_raw_rolled_bc$table))/sum(rf_caref_cf_raw_rolled_bc$table)
+
+
 # raw weighted -----------------------------------
 data <- read.csv("raw_weighted.csv")
 data <- data[,-1]
@@ -90,8 +130,6 @@ ind <- 1:23656
 
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
-
-library(randomForest)
 
 set.seed(2020)
 rf_tennis_raw_weighted <- randomForest(as.formula(paste(colnames(data)[4], "~",
@@ -111,6 +149,23 @@ table(rf_pred_raw_weighted, test_data$wl)
 
 varImpPlot(rf_tennis_raw_weighted, type = 2)
 
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_raw_weighted <- train(as.formula(paste(colnames(data)[4], "~",
+                                                          paste(colnames(data)[c(5:6, 16:75)], collapse = "+"),
+                                                          sep = "")), data = train_data,
+                                         method = 'ranger',
+                                         num.trees = 350,
+                                         trControl = ctrl,
+                                         verbose = F,
+                                         tuneGrid = rf_grid)
+
+rf_caret_pred_raw_weighted <- predict(rf_caret_tennis_raw_weighted, test_data)
+rf_caref_cf_raw_weighted <- confusionMatrix(rf_caret_pred_raw_weighted, test_data$wl)
+sum(diag(rf_caref_cf_raw_weighted$table))/sum(rf_caref_cf_raw_weighted$table)
+
 # Raw weighted BC --------------------------------
 data <- read.csv("raw_weighted_bc.csv")
 data <- data[,-1]
@@ -126,7 +181,6 @@ ind <- 1:23656
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
 
-library(randomForest)
 
 set.seed(2020)
 rf_tennis_raw_weighted_bc <- randomForest(as.formula(paste(colnames(data)[4], "~",
@@ -146,6 +200,25 @@ table(rf_pred_raw_weighted_bc, test_data$wl)
 
 varImpPlot(rf_tennis_raw_weighted_bc, type = 2)
 
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_raw_weighted_bc <- train(as.formula(paste(colnames(data)[4], "~",
+                                                        paste(colnames(data)[c(5:6, 16:75)], collapse = "+"),
+                                                        sep = "")), data = train_data,
+                                       method = 'ranger',
+                                       num.trees = 350,
+                                       trControl = ctrl,
+                                       verbose = F,
+                                       tuneGrid = rf_grid)
+
+rf_caret_pred_raw_weighted_bc <- predict(rf_caret_tennis_raw_weighted_bc, test_data)
+rf_caref_cf_raw_weighted_bc <- confusionMatrix(rf_caret_pred_raw_weighted_bc, test_data$wl)
+sum(diag(rf_caref_cf_raw_weighted_bc$table))/sum(rf_caref_cf_raw_weighted_bc$table)
+
+
+
 # Using the engineered data --------------------------------------------------------
 # engineered rolled -----------------------------------
 data <- read.csv("engineered_rolled.csv")
@@ -162,11 +235,9 @@ ind <- 1:23656
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
 
-library(randomForest)
-
 set.seed(2020)
 rf_tennis_engineered_rolled <- randomForest(as.formula(paste(colnames(data)[4], "~",
-                                                             paste(colnames(data)[c(5:6, 16:42)], collapse = "+"),
+                                                             paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
                                                              sep = "")), data = train_data, 
                                             ntree = 500, #no mtry argument, keep it default
                                             importance = TRUE, 
@@ -181,6 +252,23 @@ table(rf_pred_engineered_rolled, test_data$wl)
 (rf_err_engineered_rolled <- mean(rf_pred_engineered_rolled != test_data$wl))
 
 varImpPlot(rf_tennis_engineered_rolled, type = 2)
+
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_engineered_rolled <- train(as.formula(paste(colnames(data)[4], "~",
+                                                     paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
+                                                     sep = "")), data = train_data,
+                                    method = 'ranger',
+                                    num.trees = 350,
+                                    trControl = ctrl,
+                                    verbose = F,
+                                    tuneGrid = rf_grid)
+
+rf_caret_pred_engineered_rolled <- predict(rf_caret_tennis_engineered_rolled, test_data)
+rf_caref_cf_engineered_rolled <- confusionMatrix(rf_caret_pred_engineered_rolled, test_data$wl)
+sum(diag(rf_caref_cf_engineered_rolled$table))/sum(rf_caref_cf_engineered_rolled$table)
 
 # engineered Rolled BC --------------------------------
 data <- read.csv("engineered_rolled_bc.csv")
@@ -197,11 +285,9 @@ ind <- 1:23656
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
 
-library(randomForest)
-
 set.seed(2020)
 rf_tennis_engineered_rolled_bc <- randomForest(as.formula(paste(colnames(data)[4], "~",
-                                                                paste(colnames(data)[c(5:6, 16:42)], collapse = "+"),
+                                                                paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
                                                                 sep = "")), data = train_data, 
                                                ntree = 500, #no mtry argument, keep it default
                                                importance = TRUE, 
@@ -216,6 +302,23 @@ table(rf_pred_engineered_rolled_bc, test_data$wl)
 (rf_err_engineered_rolled_bc <- mean(rf_pred_engineered_rolled_bc != test_data$wl))
 
 varImpPlot(rf_tennis_engineered_rolled_bc, type = 2)
+
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_engineered_rolled_bc <- train(as.formula(paste(colnames(data)[4], "~",
+                                                        paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
+                                                        sep = "")), data = train_data,
+                                       method = 'ranger',
+                                       num.trees = 350,
+                                       trControl = ctrl,
+                                       verbose = F,
+                                       tuneGrid = rf_grid)
+
+rf_caret_pred_engineered_rolled_bc <- predict(rf_caret_tennis_engineered_rolled_bc, test_data)
+rf_caref_cf_engineered_rolled_bc <- confusionMatrix(rf_caret_pred_engineered_rolled_bc, test_data$wl)
+sum(diag(rf_caref_cf_engineered_rolled_bc$table))/sum(rf_caref_cf_engineered_rolled_bc$table)
 
 # engineered weighted -----------------------------------
 data <- read.csv("engineered_weighted.csv")
@@ -232,11 +335,9 @@ ind <- 1:23656
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
 
-library(randomForest)
-
 set.seed(2020)
 rf_tennis_engineered_weighted <- randomForest(as.formula(paste(colnames(data)[4], "~",
-                                                               paste(colnames(data)[c(5:6, 16:42)], collapse = "+"),
+                                                               paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
                                                                sep = "")), data = train_data, 
                                               ntree = 500, #no mtry argument, keep it default
                                               importance = TRUE, 
@@ -251,6 +352,23 @@ table(rf_pred_engineered_weighted, test_data$wl)
 (rf_err_engineered_weighted <- mean(rf_pred_engineered_weighted != test_data$wl))
 
 varImpPlot(rf_tennis_engineered_weighted, type = 2)
+
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
+
+rf_caret_tennis_engineered_weighted <- train(as.formula(paste(colnames(data)[4], "~",
+                                                       paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
+                                                       sep = "")), data = train_data,
+                                      method = 'ranger',
+                                      num.trees = 350,
+                                      trControl = ctrl,
+                                      verbose = F,
+                                      tuneGrid = rf_grid)
+
+rf_caret_pred_engineered_weighted <- predict(rf_caret_tennis_engineered_weighted, test_data)
+rf_caref_cf_engineered_weighted <- confusionMatrix(rf_caret_pred_engineered_weighted, test_data$wl)
+sum(diag(rf_caref_cf_engineered_weighted$table))/sum(rf_caref_cf_engineered_weighted$table)
 
 # engineered weighted BC --------------------------------
 data <- read.csv("engineered_weighted_bc.csv")
@@ -267,11 +385,9 @@ ind <- 1:23656
 train_data <- data[ind, ]
 test_data <- data[-ind, ]
 
-library(randomForest)
-
 set.seed(2020)
 rf_tennis_engineered_weighted_bc <- randomForest(as.formula(paste(colnames(data)[4], "~",
-                                                                  paste(colnames(data)[c(5:6, 16:42)], collapse = "+"),
+                                                                  paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
                                                                   sep = "")), data = train_data, 
                                                  ntree = 500, #no mtry argument, keep it default
                                                  importance = TRUE, 
@@ -287,66 +403,20 @@ table(rf_pred_engineered_weighted_bc, test_data$wl)
 
 varImpPlot(rf_tennis_engineered_weighted_bc, type = 2)
 
+# Ranger
+set.seed(2020)
+cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
 
+rf_caret_tennis_engineered_weighted_bc <- train(as.formula(paste(colnames(data)[4], "~",
+                                                          paste(colnames(data)[c(5:6, 16:41)], collapse = "+"),
+                                                          sep = "")), data = train_data,
+                                         method = 'ranger',
+                                         num.trees = 350,
+                                         trControl = ctrl,
+                                         verbose = F,
+                                         tuneGrid = rf_grid)
 
-# Random Forest in Ranger ----------------------------------------------------
-
-# library(caret) 
-# 
-# set.seed(1234)
-# cv_folds <- createFolds(train_data$wl, k = 5, returnTrain = TRUE)
-# 
-# ctrl <- trainControl(method = 'cv', number = 5, verboseIter = T)
-# rf_grid <- expand.grid(mtry = c(2:7),
-#                        splitrule = 'gini',
-#                        min.node.size = c(1:5))
-# 
-# set.seed(2020)
-# rf_tennis <- train(as.formula(paste(colnames(features)[4], "~",
-#                                     paste(colnames(features)[c(5:6, 16:26)], collapse = "+"),
-#                                     sep = "")), data = train_data, 
-#                    method = 'ranger',
-#                    num.trees = 250, 
-#                    trControl = ctrl, 
-#                    verbose = F, 
-#                    tuneGrid = rf_grid)
-# 
-# rf_pred <- predict(rf_tennis, test_data)
-# rf_cf <- confusionMatrix(rf_pred, test_data$wl)
-# sum(diag(rf_cf$table))/sum(rf_cf$table)
-
-
-
-
-
-# Random Forest in Ranger ----------------------------------------------------
-# library(caret) 
-# 
-# set.seed(2020)
-# cv_folds <- createFolds(train_data_features$wl, k = 5, returnTrain = TRUE)
-# 
-# ctrl <- trainControl(method = 'cv', number = 5, verboseIter = T)
-# rf_grid <- expand.grid(mtry = c(2:7),
-#                        splitrule = 'gini',
-#                        min.node.size = c(1:5))
-# 
-# set.seed(2020)
-# rf_tennis_optim_features <- train(as.formula(paste(colnames(features)[4], "~",
-#                                     paste(colnames(features)[c(5:6, 16:98)], collapse = "+"),
-#                                     sep = "")), data = train_data_features, 
-#                    method = 'ranger',
-#                    num.trees = 250, 
-#                    trControl = ctrl, 
-#                    verbose = F, 
-#                    tuneGrid = rf_grid)
-# 
-# rf_pred_optim_features <- predict(rf_tennis_optim_features, test_data_features)
-# rf_cf_of <- confusionMatrix(rf_pred_optim_features, test_data_features$wl)
-# sum(diag(rf_cf_of$table))/sum(rf_cf_of$table)
-# 
-# 
-# xtable(colnames(data))
-# 
-# players <- read.csv("players.csv")
-
+rf_caret_pred_engineered_weighted_bc <- predict(rf_caret_tennis_engineered_weighted_bc, test_data)
+rf_caref_cf_engineered_weighted_bc <- confusionMatrix(rf_caret_pred_engineered_weighted_bc, test_data$wl)
+sum(diag(rf_caref_cf_engineered_weighted_bc$table))/sum(rf_caref_cf_engineered_weighted_bc$table)
 
